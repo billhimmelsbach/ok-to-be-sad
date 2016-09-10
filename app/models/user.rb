26426e02828e5e4544ed367effaf89b2a2535dfc
billhimmelsbach <whimmels@gmail.com>
2016-09-10 05:53:02 -0700

    sets up basic structure of app with controllers and models, creates a basic seed file for testing, tests all database connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
+  has_secure_password
   before_validation :default_values
 
   # friendly_id :full_name, use: [:slugged, :finders]
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, uniqueness: true, length: { in: 2..150}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, presence: true
-  validates :first_name, length: { in: 2..50}, presence: true
-  validates :last_name, length: { in: 2..75}, presence: true
+  # validates :email, uniqueness: true, length: { in: 2..150}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, presence: true
+  # validates :first_name, length: { in: 2..50}, presence: true
+  # validates :last_name, length: { in: 2..75}, presence: true
 
 
   has_many :articles
@@ -16,8 +17,8 @@ class User < ActiveRecord::Base
   private
 
   def default_values
-    self.image = "https://ucarecdn.com/1386c488-f2db-4b63-959f-32656a7e35c6/" if self.image == ""
-    self.full_name = "#{self.first_name} #{self.last_name}"
+    # self.image = "https://ucarecdn.com/1386c488-f2db-4b63-959f-32656a7e35c6/" if self.image == ""
+    # self.full_name = "#{self.first_name} #{self.last_name}"
   end
 
 
